93c428b6ba564fbeebf7820f7095128d6eb8dea6
FeliksHelenius <felikshelenius@gmail.com>
2023-09-08 16:36:58 +0200

    sec

--- src/routers/mainRouter.jsx
+++ src/routers/mainRouter.jsx
@@ -3,30 +3,33 @@ import ErrorBoundary from './ErrorBoundary';
 import App from '../App';
 import { Landing, About, Gallery, Contact } from './routes/routes';
 
-const router = createBrowserRouter([
-	{
-		path: '/',
-		element: <App />,
-		errorElement: <ErrorBoundary />,
-		children: [
-			{
-				path: '/',
-				element: <Landing />,
-			},
-			{
-				path: '/om-oss',
-				element: <About />,
-			},
-			{
-				path: '/galleri',
-				element: <Gallery />,
-			},
-			{
-				path: '/kontakta-oss',
-				element: <Contact />,
-			},
-		],
-	},
-]);
+const router = createBrowserRouter(
+	[
+		{
+			path: '/',
+			element: <App />,
+			errorElement: <ErrorBoundary />,
+			children: [
+				{
+					path: '/',
+					element: <Landing />,
+				},
+				{
+					path: '/om-oss',
+					element: <About />,
+				},
+				{
+					path: '/galleri',
+					element: <Gallery />,
+				},
+				{
+					path: '/kontakta-oss',
+					element: <Contact />,
+				},
+			],
+		},
+	],
+	{ basename: import.meta.env.DEV ? '/' : '/soderortsmalarna' }
+);
 
 export default router;
